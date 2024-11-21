77b6075ada40f80d97630f8300effd3f373e760f
quizford <sociabarbernet@gmail.com>
2024-11-21 08:51:40 -0700

    Hide unused items

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import Link from "next/link";
+// import Link from "next/link";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
-import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+// import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { ReadCalls } from "~~/components/ReadCalls";
-import { Address } from "~~/components/scaffold-eth";
+// import { Address } from "~~/components/scaffold-eth";
 
 const Home: NextPage = () => {
-  const { address: connectedAddress } = useAccount();
+  // const { address: connectedAddress } = useAccount();
 
   return (
     <>
@@ -18,10 +18,10 @@ const Home: NextPage = () => {
             <span className="block text-2xl mb-2">Welcome to</span>
             <span className="block text-4xl font-bold">⛓️ ChainSpeed ⛓️</span>
           </h1>
-          <div className="flex justify-center items-center pt-5 space-x-2 flex-col sm:flex-row">
+          {/* <div className="flex justify-center items-center pt-5 space-x-2 flex-col sm:flex-row">
             <p className="my-2 font-medium">Connected Address:</p>
             <Address address={connectedAddress} />
-          </div>
+          </div> */}
         </div>
         <div className="pt-10">
           <ReadCalls />
@@ -29,7 +29,7 @@ const Home: NextPage = () => {
 
         <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
           <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+            {/* <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
               <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
               <p>
                 Explore your local transactions with the{" "}
@@ -38,7 +38,7 @@ const Home: NextPage = () => {
                 </Link>{" "}
                 tab.
               </p>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
